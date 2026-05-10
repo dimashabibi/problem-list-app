eefd145ca71b74a37a1cd13f316f366cc0521a6e
dimashabibi <dimsshidayat28@gmail.com>
2026-05-11 00:19:54 +0700

    fixing css

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -1,5 +1,7 @@
 <link href="{{ asset('assets/css/vendor.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/app.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
-<link href="{{ asset('assets/vendor/datatables/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/vendor/datatables/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet"
+    type="text/css" />
 <link href="{{ asset('assets/vendor/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
 <script src="{{ asset('assets/js/config.js') }}"></script>
